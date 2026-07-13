3fc7e0d2e5e11caa7277d492378e2625685007cc
samcole8 <sam@samcole.me>
2026-07-13 22:29:51 +0100

    chore: remove old system and contract implementations

--- src/ice/contract.py
+++
@@ -1,9 +0,0 @@
-class Contract:
-    def __init__(self, src, dst, lcl, expression):
-        self.src = src
-        self.dst = dst
-        self.lcl = lcl
-        self.expression = expression
-    
-    def evaluate(self):
-        return self.expression(self.src, self.dst, self.lcl)
