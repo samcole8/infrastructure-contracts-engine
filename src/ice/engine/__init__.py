69869ff16728d5fa30d079b52aed5e92dca71b93
samcole8 <sam@samcole.me>
2026-07-14 12:41:42 +0100

    feat: implement `Connection` and `Probe`

--- src/ice/engine/__init__.py
+++ src/ice/engine/__init__.py
@@ -1,2 +1,3 @@
 from ice.engine.engine import Engine
 from ice.engine.builder import build
+from ice.engine.probe import Probe, Connection, LocalConnection
--- src/ice/engine/probe.py
+++ src/ice/engine/probe.py
@@ -1,11 +1,18 @@
+import subprocess
+from contextlib import contextmanager
+from fabric import Connection as FabricConnection
+
+def open_ssh(target, username, password):
+    return FabricConnection(host=target, user=username, connect_kwargs={"password": password})
+
 class Probe:
     
     def __init__(self, capability, script):
         self.capability = capability
         self.script = script
     
-    def run(self, session):
-        pass
+    def run(self, runner):
+        self.capability.state = runner(self.script)
 
 class Connection:
     def __init__(self, system, target, username, password):
@@ -15,18 +22,32 @@ class Connection:
         self.password = password
         self.probes = []
 
-    def run(self):
-        with self.connect() as session:
-            for probe in self.probes:
-                probe.run(session)
-    
+    @contextmanager
     def connect(self):
-        pass
+        client = open_ssh(self.target, self.username, self.password)
+        try:
+            def runner(script):
+                result = client.run(script, hide=True, warn=True)
+                return result.return_code == 0
+            yield runner
+        finally:
+            client.close()
+    
+    def run(self):
+        if self.probes:
+            with self.connect() as session:
+                for probe in self.probes:
+                    probe.run(session)
 
 class LocalConnection(Connection):
     
     def __init__(self):
-        super().__init__(system=None, target=None, username=None, password=None)
+        super().__init__(None, None, None, None)
+        self.probes = []
 
+    @contextmanager
     def connect(self):
-        pass
+        def runner(script):
+            result = subprocess.run(["bash", "-c", script], capture_output=True)
+            return result.returncode == 0
+        yield runner
