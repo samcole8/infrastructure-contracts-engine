6b74f545ce138d2aea0aafe787d6b5a33cab43d3
samcole8 <sam@samcole.me>
2026-07-15 22:46:18 +0100

    feat: main loop

--- src/ice/main.py
+++ src/ice/main.py
@@ -1,51 +1,25 @@
 from ice.api import API
-from ice.engine import Engine, build
+from ice.engine.builder import build
+from ice.engine import Engine
+from ice.reporting import terraform
 
 INTERVAL = 60
 
-# Temporary configuration
-CONFIG = {
-  "systems": [
-    {"name": "system_1"},
-    {"name": "system_2"}
-  ],
-
-  "capabilities": [
-    {
-      "name": "cap_1",
-      "src": "system_1",
-      "dst": "system_2",
-      "origin": "dst",
-      "check": "cap_1_check"
-    },
-    {
-      "name": "cap_2",
-      "src": "system_1",
-      "dst": "system_2",
-      "state": True
-    }
-  ],
-
-  "requirements": [
-    {
-      "name": "requirement_1",
-      "src": "system_2",
-      "contract": "cap_1 or cap_2"
-    }
-  ]
-}
-
 def main():
-    # Start API
+
     api = API()
     engine = Engine(INTERVAL)
-
+    
     while True:
+
         # Get systems and probes
-        systems, connections = build(CONFIG) # API config input blocks once implemented
-        if not engine.reload(systems, connections):
-            # Failure, invoke API response
-            break
+        config = api.listen()
+        systems = build(config)
+        errors = engine.reload(systems)
+        if errors:
+            api.respond(terraform.response(errors))
+        else:
+            api.respond(None)
 
 if __name__ == "__main__":
     main()
